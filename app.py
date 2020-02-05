cab7cbec44ff592345605bc55e1d1ed40ba0f398
terencecistudent <terencelogue100@hotmail.co.uk>
2020-02-05 16:39:10 +0000

    requirements.txt updated, app.py changed to environment variable

--- app.py
+++ app.py
@@ -1,19 +1,23 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
-from bson.objectid import ObjectId 
+from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
-
-app.config["MONGO_DBNAME"] = "task_manager"
-app.config["MONGO_URI"] = 'mongodb+srv://root-tl:Coda2015@myfirstcluster-dzwbf.mongodb.net/task_manager?retryWrites=true&w=majority'
-
+app.config["MONGODB_NAME"] = os.environ.get('MONGODB_NAME')
+app.config["MONGO_URI"] = os.environ.get('MONGO_URI')
 mongo = PyMongo(app)
+
+DBS_NAME = "task_manager"
+
 
 @app.route("/")
 @app.route("/get_tasks")
 def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
 
 if __name__ == "__main__":
     app.run(host=os.environ.get('IP'),
