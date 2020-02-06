088d710c9a85cc194dcae12f8c1c952f0985fb09
terencecistudent <terencelogue100@hotmail.co.uk>
2020-02-06 13:21:36 +0000

    Adding tasks

--- app.py
+++ app.py
@@ -19,9 +19,20 @@ def get_tasks():
     return render_template("tasks.html", tasks=mongo.db.tasks.find())
 
 
+# used the find function with MongoDB to fetch our categories.
 @app.route("/add_task")
 def add_task():
-    return render_template("addtask.html")
+    return render_template("addtask.html", categories=mongo.db.categories.find())
+
+
+# use POST method to deliver form data
+# go back to mongo and get the task collections (line 33)
+# insert_one request form and coverting form to dict (line 35)
+@app.route("/insert_track", methods=["POST"])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict())
+    return redirect(url_for("get_tasks"))
 
 
 if __name__ == "__main__":
